7b1bb010a28245111f62d8ee8bc5af3b95f1e1c2
KNSaiShraddha <saishraddhakn@gmail.com>
2025-07-15 09:48:49 +0530

    Update stepic.py

--- stepic.py
+++ stepic.py
@@ -1,49 +1,46 @@
 from PIL import Image
 
-def _int_to_bin(rgb):
-    return tuple(format(c, '08b') for c in rgb)
+def _text_to_bin(text):
+    return ''.join(format(ord(c), '08b') for c in text)
 
-def _bin_to_int(rgb):
-    return tuple(int(c, 2) for c in rgb)
+def _bin_to_text(binary):
+    chars = [binary[i:i+8] for i in range(0, len(binary), 8)]
+    return ''.join(chr(int(b, 2)) for b in chars)
 
-def _merge_rgb(rgb, bits):
-    r, g, b = _int_to_bin(rgb)
-    new_rgb = r[:-1] + bits[0], g[:-1] + bits[1], b[:-1] + bits[2]
-    return _bin_to_int(new_rgb)
-
-def encode(img, data):
-    img = img.convert('RGB')
-    encoded = img.copy()
-    width, height = img.size
-    data += chr(0)  # Null char to indicate end of message
-    data_bits = ''.join(format(ord(c), '08b') for c in data)
+def encode(image, text):
+    binary = _text_to_bin(text) + '1111111111111110'  # EOF marker
+    encoded = image.copy()
+    pixels = encoded.load()
+    width, height = encoded.size
     data_index = 0
 
     for y in range(height):
         for x in range(width):
-            if data_index + 3 <= len(data_bits):
-                rgb = img.getpixel((x, y))
-                bits = data_bits[data_index:data_index+3]
-                data_index += 3
-                encoded.putpixel((x, y), _merge_rgb(rgb, bits))
-            else:
+            if data_index >= len(binary):
                 return encoded
+            r, g, b = pixels[x, y]
+            r = (r & ~1) | int(binary[data_index])
+            data_index += 1
+            if data_index < len(binary):
+                g = (g & ~1) | int(binary[data_index])
+                data_index += 1
+            if data_index < len(binary):
+                b = (b & ~1) | int(binary[data_index])
+                data_index += 1
+            pixels[x, y] = (r, g, b)
     return encoded
 
-def decode(img):
-    img = img.convert('RGB')
-    width, height = img.size
-    bits = ''
+def decode(image):
+    binary = ''
+    pixels = image.load()
+    width, height = image.size
 
     for y in range(height):
         for x in range(width):
-            r, g, b = _int_to_bin(img.getpixel((x, y)))
-            bits += r[-1] + g[-1] + b[-1]
-
-    chars = [bits[i:i+8] for i in range(0, len(bits), 8)]
-    message = ''
-    for c in chars:
-        if int(c, 2) == 0:
-            break
-        message += chr(int(c, 2))
-    return message
+            r, g, b = pixels[x, y]
+            binary += str(r & 1)
+            binary += str(g & 1)
+            binary += str(b & 1)
+            if binary[-16:] == '1111111111111110':  # EOF marker
+                return _bin_to_text(binary[:-16])
+    return _bin_to_text(binary)
